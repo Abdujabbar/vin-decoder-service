49b0d51424dcec7de2e90c0b749719124b2e5633
Abdujabbor <abdujabbor1987@gmail.com>
2018-08-20 16:41:24 +0500

    moved business logic to model

--- service/models.py
+++ service/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from .transport.decodethis import DecodeThisTransport
+from .decoder.decodethis import *
+from django.conf import settings
 
 
 class Vehicle(models.Model):
@@ -15,3 +17,20 @@ class Vehicle(models.Model):
     def __str__(self):
         return self.vin
 
+    @staticmethod
+    def get_or_create(vin):
+        try:
+            record = Vehicle.objects.get(vin=vin)
+            return record
+        except models.ObjectDoesNotExist as e:
+            pass
+        transport = DecodeThisTransport(settings.DECODE_THIS_URL %
+                                        (
+                                            vin,
+                                            settings.DECODE_API_KEY,
+                                            settings.DECODE_THIS_JSON_FORMAT
+                                        ), [])
+
+        data = transport.lunch_request()
+        vehicle_dict = DecodeThisDecoder(data).run()
+        return Vehicle.objects.create(**vehicle_dict)
--- service/tests.py
+++ service/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+import unittest
+from .transport.decodethis import DecodeThisTransport
 # Create your tests here.
+class DecodeThisTransportTest(TestCase):
+    def test_invalid_arguments(self):
+        with self.assertRaises(Exception) as cm:
+            DecodeThisTransport("", [])
+
+        with self.assertRaises(Exception) as c:
+            DecodeThisTransport("dsadsa", [])
+
