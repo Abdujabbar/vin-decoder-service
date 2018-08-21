141e5f4b6e4ec2e604b01967b301d64c763ef581
Abdujabbor <abdujabbor1987@gmail.com>
2018-08-22 01:45:56 +0500

    swagger added

--- app/urls.py
+++ app/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
 
+schema_view = get_swagger_view(title='Decoder API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     path('decode/', include('service.urls')),
     path('admin/', admin.site.urls)
 ]
